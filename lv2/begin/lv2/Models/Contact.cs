02ea1e98826c638fdf4e98602e56cc9d465f933b
rlaby <rlaby5@gmail.com>
2016-04-20 16:47:51 +0200

    [dodana klasa]

---
+++ lv2/begin/lv2/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lv2.Models
+{
+    public class Contact
+    {
+        
+            public class Contact
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
+    }
+}
