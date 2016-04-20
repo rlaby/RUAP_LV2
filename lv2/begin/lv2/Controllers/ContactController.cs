0b20985f22e716202acb997c9ac10da96c0044f9
rlaby <rlaby5@gmail.com>
2016-04-20 17:20:38 +0200

    [gotova zadnja tocka prvog zad]

--- lv2/begin/lv2/Controllers/ContactController.cs
+++ lv2/begin/lv2/Controllers/ContactController.cs
@@ -5,27 +5,22 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using lv2.Models;
+using lv2.Services;
 
 namespace lv2.Controllers
 {
     public class ContactController : ApiController
     {
+        private ContactRepository contactRepository;
 
-        public Contact[] Get()
+        public ContactController()
         {
-            return new Contact[]
-            {
-                new Contact
-                {
-                    Id = 1,
-                    Name = "Glenn Block"
-                },
-                new Contact
-                {
-                    Id = 2,
-                    Name = "Dan Roth"
-                }
-            };
+            this.contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get() { 
+        return this.contactRepository.GetAllContacts();
         }
     }
 }
---
+++ lv2/begin/lv2/Services/ContactRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using lv2.Models;
+
+namespace lv2.Services
+{
+    public class ContactRepository
+    {
+        public Contact[] GetAllContacts()
+        {
+            return new Contact[]
+        {
+             new Contact
+             {
+                  Id = 1,
+                  Name = "Glenn Block"
+             },
+             new Contact
+             {
+                  Id = 2,
+                  Name = "Dan Roth"
+             }
+        };
+        }
+    }
+}
+
