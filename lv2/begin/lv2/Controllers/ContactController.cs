7152461d3323bee1a132c9b3ed9e287340bf5b34
rlaby <rlaby5@gmail.com>
2016-04-20 16:36:40 +0200

    [dodan contract_controller]

---
+++ lv2/begin/lv2/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace lv2.Controllers
+{
+    public class ContactController : ApiController
+    {
+
+        public string[] Get()
+        {
+            return new string[]
+        {
+             "Hello",
+             "World"
+        };
+        }
+
+
+    }
+}
