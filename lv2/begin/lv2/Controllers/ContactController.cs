4dd7ca881d9aace807a2d3d68ec349131f02585e
rlaby <rlaby5@gmail.com>
2016-04-20 18:26:46 +0200

    [final]

--- lv2/begin/lv2/Controllers/ContactController.cs
+++ lv2/begin/lv2/Controllers/ContactController.cs
@@ -23,9 +23,20 @@ namespace lv2.Controllers
             return contactRepository.GetAllContacts();
         }
 
-        
 
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
 
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
 
+            return response;
+        }
+    
+    
+    
+    
     }
+
+
 }
